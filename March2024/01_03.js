a854d0015cd0f764d00dd6abc9d9b16b1952e3fa
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-01 19:32:19 +0530

    all functions are done

--- March2024/01_03.js
+++ March2024/01_03.js
@@ -1,1 +1,48 @@
 // sees the all types of functions in js
+// function declarations 
+// function expression
+// Arrow functions
+
+
+
+
+// function add(num1,num2){
+//     return num1+num2;
+// }
+// console.log(add(2,3))
+
+// const add = function(num1,num2){
+//     return num1+num2;
+// }
+// console.log(add(2,3))
+
+
+// const add =(num1,num2)=>{
+//     return num1+num2;
+// }
+// console.log(add(2,3))
+
+
+
+
+const str = myStr => myStr[0];
+console.log(str("Ram"))
+
+
+
+
+
+
+
+// function isEven(number){
+//     return number % 2 == 0;
+// }
+// console.log(isEven(4))
+
+// const isEven = function (number){
+//     return number % 2 == 0;
+// }
+// console.log(isEven(4))
+
+// const isEven = number => number%2==0;
+// console.log(isEven(4))
