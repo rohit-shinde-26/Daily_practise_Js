6b07db6427d6a742e96d56730fb6ef068c78b07b
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-02 21:42:29 +0530

    done for day a 50%

---
+++ March2024/02_03.js
@@ -0,0 +1,106 @@
+// console.log("jay shree ram")
+// var a = 90;
+// var name = "ram";
+// let myName = "Rohit";
+// const email = "rohitshinde@gmail.com";
+// console.log(name[0])
+// console.log(email.charAt(2))
+// console.log(email.indexOf("@"))
+// console.log(`my name is ${name} and email is ${email}`)
+
+// let a = 0;
+// let ans = a ? "true value" : "falsy value";
+// console.log(ans)
+
+// let winningNum = 20;
+// let userGuess = +prompt("Guess the number");
+// if(winningNum === userGuess){
+//     console.log("Congratulations you are win the game")
+// }
+// else{
+//     if(userGuess < winningNum){
+//         console.log("you are thinking less")
+//     }else{
+//         console.log("you are thinking high")
+//     }
+// }
+
+// let day = 2;
+// switch(day){
+//     case 1:
+//         console.log("Tuesday");
+//         break;
+//     case 2:
+//         console.log("wednesday");
+//         break;
+//     case 3:
+//         console.log("Thursday");
+//         break;
+
+//         case 4:
+//             console.log("Friday");
+//             break;
+//     default :
+//         console.log("Invalid format");
+//         break;
+// }
+
+// let i = 0;
+// let total = 0;
+// while(i<10){
+//     console.log(i)
+//     total = total + i;
+//     i++;
+// }
+// console.log(total)
+
+// for(let i=0;i<10;i++){
+//     console.log(i)
+// }
+
+// for(let i=0;i<10;i++){
+//         console.log(i)
+//         if(i == 5){
+//             break;
+//         }
+//         else{
+            
+//         }
+//      }
+
+
+const myArr = ["ram","sham","abhi","raj"]
+// myArr.push("Akash")
+// myArr.unshift("Avni")
+// console.log(myArr);
+
+// const cloneArr = [...myArr];
+// console.log(myArr === cloneArr);
+
+// const arr1 = ["TATA","AUDI","BMW","VOLVO"];
+// let [val1, ,val2,...otherVal] = arr1;
+// console.log(val2)
+
+
+// const theArr =[];
+// for(let i=0;i<myArr.length;i++){
+//     theArr.push(myArr[i].toUpperCase())
+// }
+// console.log(theArr);
+
+// for(let val of myArr){
+//     console.log(val)
+// }
+// for(let val in myArr){
+//     console.log(myArr[val])
+// }
+
+const myObj={
+    name : "Rohit",
+    age:20,
+    intrest : "coading",
+}
+console.log(myObj);
+
+console.log(myObj.name)
+console.log(myObj["intrest"])
