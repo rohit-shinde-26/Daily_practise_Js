19b781c768976fe26461555b89f02d9b61672850
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-03 19:25:18 +0530

    function

--- March2024/03_03.js
+++ March2024/03_03.js
@@ -1,11 +1,27 @@
-function findTarget(arr1,target){
-    for(let i=0;i<arr1.length;i++){
-    if(arr1[i] === target){
-        return i
-    }
-}
-    return "hello"
-}
-const arr1 = [23,56,78,45,15];
+// function findTarget(arr1,target){
+//     for(let i=0;i<arr1.length;i++){
+//     if(arr1[i] === target){
+//         return i
+//     }
+// }
+//     return "hello"
+// }
+// const arr1 = [23,56,78,45,15];
 
-console.log(findTarget(arr1,15))
+// console.log(findTarget(arr1,15))
+
+// hello();
+// function hello(){
+//     console.log("hello world")
+// }
+
+// hello();
+// const hello =()=>{
+//     console.log("hello world")
+// }
+
+const app =() =>{
+    const isEven = num => num%2 == 0;
+    console.log(isEven(8))
+}
+app();
