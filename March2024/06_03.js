57971a04721b54110090d2107b287d12853e467c
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-06 19:49:01 +0530

    functions

---
+++ March2024/06_03.js
@@ -0,0 +1,27 @@
+// functions 
+// function declarations 
+// function isEven(){
+//     return "Hello World"
+// }
+// console.log(isEven())
+
+// Function expression 
+// const addition = function(num1,num2){
+//     return num1+num2;
+// }
+// console.log(addition(5,4))
+
+
+// arrow funtion
+// const strIndex = str => str[0];
+// console.log(strIndex("Ram"))
+
+// hoisted function declaration 
+// console.log(isEven())
+// function isEven(){
+//     return "Hello World"
+// }
+
+
+
+// lexical scope 
