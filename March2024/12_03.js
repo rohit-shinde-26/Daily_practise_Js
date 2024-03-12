48033617b92965d08751a7d65f7646a9a1d65736
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-12 20:18:02 +0530

    functions

---
+++ March2024/12_03.js
@@ -0,0 +1,16 @@
+// declaration 
+function sub (num1,num2){
+    return num1 - num2;
+}
+console.log(sub(4,5))
+
+// Expression
+const add = function(no1,no2){
+    return no1+no2;
+}
+console.log(add(5,6))
+
+
+// Arrow fun
+const check = str => str[0]
+console.log(check("ram"))
