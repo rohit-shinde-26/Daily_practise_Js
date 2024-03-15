0f387bec651b309b1db6ada0af71aa98aa6e340d
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-15 11:27:01 +0530

    callback

--- March2024/15_03.js
+++ March2024/15_03.js
@@ -29,3 +29,28 @@
 // }
 // restFun(5,6,9,8,7,1,2,3)
 
+// parameter destructuring 
+const myObj={
+    name : "Rohit",
+    age : 21,
+    core : "Javascript"
+}
+// const myInfo = (object) => {
+//     console.log(object.name)
+// }
+const myInfo = ({name,age}) => {
+    console.log(name)
+}
+
+myInfo(myObj)
+
+// callback functions 
+const myFun1 = (age) => {
+    console.log(`my age is ${age}`)
+
+}
+const myFun2 = (callback) => {
+    console.log(`Here is the fun 2`)
+    callback(21)
+}
+myFun2(myFun1)
