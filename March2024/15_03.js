aab0d6d5ba8ba1df8ad43aafd5e4cefae8261550
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-15 08:03:41 +0530

    default and rest para

---
+++ March2024/15_03.js
@@ -0,0 +1,31 @@
+// default para 
+// works only second value 
+// function add(a,b){
+//     if(typeof b === "undefined"){
+//         b = 2;
+//     }
+//     return a+b;
+// }
+// const ans = add(5)
+// console.log(ans)
+
+// function add(a,b=2){
+//     return a+b;
+// }
+// const ans = add(5)
+// console.log(ans)
+
+// Rest parameter 
+// const normalFun = (a,b,...c) => {
+//     console.log(`value of a is ${a}`)
+//     console.log(`value of b is ${b}`)
+//     console.log(`value of c is ${c}`)
+
+// }
+// normalFun(5,6,4,3,8,5,3)
+
+// const restFun = (...number) => {
+//     console.log(`${number}`)
+// }
+// restFun(5,6,9,8,7,1,2,3)
+
