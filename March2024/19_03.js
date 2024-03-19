0810ddc2cf1afd23b8198ad524fcc1fd8f840506
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-19 19:13:20 +0530

    switch statement

--- March2024/19_03.js
+++ March2024/19_03.js
@@ -20,24 +20,45 @@
 //     console.log("Falsy value")
 // }
 
-let age = 20;
+// let age = 20;
 // age < 25 ? console.log("you are young") : console.log("You are old")
 
-if(age>0 && age <15){
-    console.log("You are child")
-}
-else if(age>15 && age <25){
-    console.log("You are Young")
-}
+// if(age>0 && age <15){
+//     console.log("You are child")
+// }
+// else if(age>15 && age <25){
+//     console.log("You are Young")
+// }
 
-else if(age>25 && age <45){
-    console.log("You are men")
-}
+// else if(age>25 && age <45){
+//     console.log("You are men")
+// }
 
-else if(age>45 && age <65){
-    console.log("You are old")
-}
-else{
-    console.log("Near to death")
-}
+// else if(age>45 && age <65){
+//     console.log("You are old")
+// }
+// else{
+//     console.log("Near to death")
+// }
 
+// let day = "Monday";
+
+// switch(day){
+//     case "Sunday":
+//         console.log("Holiday")
+//         break;
+//     case "Monday":
+//         console.log("Mahadev temple")
+//         break;
+//     case "Tuesday":
+//         console.log("Play kabaddi")
+//         break;
+//     case "Thursday":
+//         console.log("pray")
+//         break;
+//     case "Friday":
+//          console.log("Travel ")
+//         break;
+//     default :
+//     console.log("invalid")
+// }
