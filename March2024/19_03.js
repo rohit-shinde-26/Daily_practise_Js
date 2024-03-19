686d8d45a22b4b3f52fd4cb5e4f1fe806ab9f549
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-19 19:01:24 +0530

    near to operator

---
+++ March2024/19_03.js
@@ -0,0 +1,12 @@
+// console.log("Hello World")
+// let myStr = "Hello Mam"
+// console.log(myStr[2])
+// const name = " Rohit"
+// let email = "rohitshinde@gmail.com"
+
+// console.log(`hey , my name is ${name} and email is ${email}`)
+
+// let myVal =null;
+// let score;
+// let fine = 651461464646464614361461n;
+// console.log(typeof (fine))
