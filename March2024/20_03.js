5da45cafef6395c9dec19d40e36b6c031b87c832
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-20 19:30:21 +0530

    while in array

---
+++ March2024/20_03.js
@@ -0,0 +1,23 @@
+// Array
+// const myArr = ["ram","Sham","Hari","Om"]
+// myArr.push("Abhi")
+// console.log(myArr)
+
+// const[...newArr] =myArr;
+// console.log(newArr)
+// console.log(myArr===newArr)
+
+// const favcar = ["Mustang Gt","Civic","Verna","Defender"]
+// const showRoom =[]
+// for(let i=0;i<favcar.length;i++){
+//     showRoom.push(favcar[i].toUpperCase())
+// }
+// console.log(showRoom)
+
+
+let i = 0;
+const bikes = ["H2","Zx10R","Hayabusa","GT 650"]
+while(i<bikes.length){
+    console.log(bikes[i])
+    i++
+}
