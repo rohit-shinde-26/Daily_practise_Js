64ee13e969d527fc74f19a782099699d518d3a3f
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-21 19:01:54 +0530

    done for the day

---
+++ March2024/21_03.js
@@ -0,0 +1,15 @@
+// for each
+
+const myArr =[2,4,6,8,10]
+
+// const myAns =(number,index)=>{
+//     console.log(`no is ${number} and index is ${index}`)
+// }
+
+// for(let i=0;i<myArr.length;i++){
+//     myAns(myArr[i],i)
+// }
+
+myArr.forEach((num,index)=>{
+    console.log(num,index)
+})
