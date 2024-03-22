5a262e6f468f19d7a0486d3f015987185a336192
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-22 10:02:20 +0530

    forEach

---
+++ March2024/22_03.js
@@ -0,0 +1,11 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+// for(let i=0;i<numbers.length;i++){
+//     console.log(numbers[i]*2)
+// }
+
+// const ans = numbers.forEach((number,index)=>{
+//     console.log(`${number} and miltiply is ${number*2}`)
+// })
+// const finalAns =ans
+// console.log(finalAns)
