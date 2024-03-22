0a6b72e197972d99463bcb4eefd1800661dfecb9
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-22 10:13:05 +0530

    map method

--- March2024/22_03.js
+++ March2024/22_03.js
@@ -9,3 +9,37 @@ const numbers = [1,2,3,4,5,6,7,8,9,10]
 // })
 // const finalAns =ans
 // console.log(finalAns)
+
+// const ans = numbers.map((number,index)=>{
+//     return number*number
+// })
+// const finalAns = ans
+// console.log(finalAns)
+
+// const ans = numbers.filter((number,index)=>{
+//     return number%2==0
+// })
+// console.log(ans)
+
+
+const friends = [
+    {fName : "Ram", lName : "Shinde", age : 21},
+    {fName : "Abhi", lName : "Gunjal", age : 25},
+    {fName : "Akshay", lName : "Rasne", age : 18},
+    {fName : "Atharv", lName : "Varpe", age : 21},
+
+]
+
+// const ans = friends.forEach((data,index)=>{
+//     console.log(data.fName.toUpperCase())
+// })
+
+// const ans = friends.map((data,index)=>{
+//     return data.lName
+// })
+// console.log(ans)
+
+const ans = friends.filter((data,index)=>{
+    return data.age>20
+})
+console.log(ans)
