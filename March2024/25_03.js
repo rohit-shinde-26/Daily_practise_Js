5c74d006118f1cf3ef47390147ecb953c2e4211d
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-25 16:11:14 +0530

    done for the day

--- March2024/25_03.js
+++ March2024/25_03.js
@@ -35,3 +35,52 @@ const myArr = ["Hello","Ram","Abhay"]
 //     return data%2===0
 // })
 // console.log(ans)
+
+
+
+
+
+
+const showRoom =[
+    {id : 1, name : "mustang", color : "red", price : 500},
+    {id : 2, name : "Lambo", color : "Yellow", price : 5000},
+    {id : 3, name : "Supra", color : "White", price : 5070},
+    {id : 4, name : "polo GT", color : "Blue", price : 600},
+    {id : 5, name : "Virtus", color : "Silver", price : 900},
+    {id : 6, name : "City", color : "Black", price : 1},
+
+]
+
+// showRoom.forEach(function(val,index){
+//     console.log(`the index is ${index} and name is ${val.name}`)
+// })
+
+// const ans = showRoom.map((Data)=>{
+//     return Data.price * Data.price
+// })
+// console.log(ans)
+
+// const ans = showRoom.filter((data)=>{
+//     return data.price%2==0
+// })
+// console.log(ans)
+
+// const ans = showRoom.reduce((data,totalVal)=>{
+//     return data + totalVal.price
+// },0)
+// console.log(ans)
+
+// const ans = showRoom.sort((a,b)=>{
+//     return a.price - b.price
+// })
+// console.log(ans)
+
+// const ans = showRoom.find((data)=>{
+//     return data.price == 900
+// })
+// console.log(ans)
+
+// const ans = showRoom.every((data)=>{
+//     return data.price%2==0
+// })
+// console.log(ans)
