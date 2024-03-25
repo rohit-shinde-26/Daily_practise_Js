eccda260b0f9eeb9a17cef5fc632bbd07be29b7a
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-25 09:27:26 +0530

    phase 1 is here for today

---
+++ March2024/25_03.js
@@ -0,0 +1,37 @@
+// Array methods
+const arr = [2,,6,4,56,56,14]
+const myArr = ["Hello","Ram","Abhay"]
+
+// arr.forEach(function(number,index){
+//     console.log(`index is ${index} number is ${number}`);
+// });
+
+// const ans = arr.map((num)=>{
+//     return num*num
+// })
+// console.log(ans)
+
+// const ans = arr.filter((num)=>{
+//     return num%2==0
+// })
+// console.log(ans)
+
+// const ans = arr.reduce((accumulator , total)=>{
+//     return accumulator + total
+// })
+// console.log(ans)
+
+// const ans = arr.sort((a,b)=>{
+//     return a-b
+// })
+// console.log(ans)
+
+// const ans = myArr.find((str)=>{
+//     return str.length===3
+// })
+// console.log(ans)
+
+// const ans =  arr.every((data)=>{
+//     return data%2===0
+// })
+// console.log(ans)
