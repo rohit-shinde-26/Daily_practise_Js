fb1cbb5abe814680440d6fd4921e3e577f8ff4e7
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-31 07:54:06 +0530

    basics done

---
+++ March2024/31_03.js
@@ -0,0 +1,66 @@
+// console.log("Hello")
+// var name = "Rohit"
+// let age = 21
+// console.log(name[0])
+// console.log(name)
+// console.log(`my name is ${name} and age is ${age}`)
+
+// let a ;
+// let b = null
+
+// console.log(typeof )
+
+// let age = 21;
+// age > 20 ? console.log("greater") : console.log("Less")
+
+// let a = 1
+// if(a){
+//     console.log("truthy")
+// }
+// else{
+//     console.log("falsy")
+// }
+
+// let age = 22
+// if(age>0 && age<15){
+//     console.log("Children")
+// }
+// else if(age>15 && age < 35){
+//     console.log("Young")
+// }
+// else{
+//     console.log("old")
+// }
+
+// let a = String( prompt("Guess the day"))
+
+// switch(a){
+//     case "Monday":
+//         console.log("its Monday")
+//     break;
+
+//     case "Sunday" :
+//         console.log("Its sunday")
+
+//     default:
+//         console.log("ends here")
+// }
+
+// let count = 0
+// let i = 0;
+// while(i<10){
+//     console.log(i)
+//     i++
+//     count = count + i
+// }
+// console.log(count)
+
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+//     if(i===6){
+//         continue;
+//     }
+    
+    
+// }
+
