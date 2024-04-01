dbd97d05197256035156deaed94b3e56178a1632
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-01 21:52:02 +0530

    bad practise

--- April2024/01_04.js
+++ April2024/01_04.js
@@ -1,1 +1,42 @@
-console.log("April")
+// console.log("April")
+
+// // sets 
+// const a = new Set()
+// a.add(9)
+// a.add(2)
+// a.add(["ram","sham"])
+
+
+// console.log(a)
+
+// // maps
+
+// const obj = {
+//     name:"rohit",
+//     age : 22
+// }
+
+// const b = new Map();
+// b.set('firstname','rohit')
+// b.set(obj, {email : "rohit@123"})
+
+// console.log(b)
+
+
+const person1 = {
+    id: 1,
+    firstName: "Rohit"
+}
+const person2 = {
+    id: 2,
+    firstName: "Aki"
+}
+
+// const extraInfo = new Map();
+// extraInfo.set(person1, {age: 8, gender: "male"});
+// extraInfo.set(person2, {age: 9, gender: "female"});
+// // console.log(userInfo);
+// console.log(person1.id);
+// console.log(extraInfo.get(person1).gender);
+// console.log(extraInfo.get(person2).gender);
+
