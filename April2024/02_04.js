562c833fba0184c08f4f8b5ff5f6628f4d2e648c
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-02 11:44:24 +0530

    sets completed

---
+++ April2024/02_04.js
@@ -0,0 +1,52 @@
+// sets
+// const numbers = new Set([1,2,3,4,5,5]) // duplicate not allow
+// console.log(numbers)
+
+// index based not accessed
+// console.log(numbers[0])
+
+// const numbers = new Set() 
+// numbers.add(1)
+// numbers.add(2)
+// numbers.add(3)
+// numbers.add(4)
+// numbers.add(5)
+// numbers.add(6)
+// numbers.add(7)
+// const items = ["ram","sham"]
+// numbers.add(items)
+// numbers.add("Hello")
+
+
+// for(let number of numbers){
+//     console.log(number)
+// }
+
+// if(numbers.has(1)){
+//     console.log(" 1 is present")
+// }
+// else{
+//     console.log("not present")
+// }
+// console.log(numbers)
+
+
+// you can use this for unique ids
+
+// const myArr = [2,5,4,5,8,2,6]
+// const ans = new Set(myArr);
+// let length = 0
+// for(let arr of ans){
+//     console.log(arr)
+//     length++
+// }
+
+// console.log(`length is ${length}`)
+
+
+// Map object
+// it store key value pair 
+
+
+
+
