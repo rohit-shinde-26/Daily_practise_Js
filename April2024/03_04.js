5f7c4e6b49344eb65175a7de556913112fa66a8e
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-03 13:11:32 +0530

    done for todaY

---
+++ April2024/03_04.js
@@ -0,0 +1,68 @@
+// iterables 
+// iterables are the type in that length property was used also for loop
+
+
+// sets 
+// const person = new Set("abc")
+// console.log(person)
+
+// const data = new Set()
+// data.add(4)
+// data.add(3)
+// data.add(["ram","sham"])
+
+// console.log(data)
+
+// for(let key of data){
+//     console.log(key, typeof key)
+// }
+
+
+// map 
+// const person1 = {
+//     firstName : "rohit",
+//     age : 22,
+//     1 : "hello"
+// }
+// const info = new Map([["firstName","Ram"],["Age",22],[1,"hello"]])
+// console.log(info)
+
+// for(let [key,val] of info){
+//     console.log(key,typeof key)
+// }
+
+// const data = new Map();
+// data.set(person1)
+
+// console.log(person1)
+// console.log(data.set(person1,{gmail : "ram@123"}))
+// console.log(data.get(person1).gmail)
+
+// object assign
+// const person = {
+//     name : "ram",
+//     age : 22
+// }
+// const info = Object.assign({},person)
+// const info = {...person}
+// console.log(info)
+
+// console.log(person===info)
+
+// optional chaining 
+
+// const data = {
+//     firstName : "ram",
+//     // address : { one : "hello"}
+// }
+
+// // console.log(data.firstName)
+// console.log(data?.address?.one)
+
+
+// const person = {
+//     name : "Rohit",
+//     // info: {address : "sangamner"}
+// }
+// console.log(person.name)
+// console.log(person?.info?.address)
