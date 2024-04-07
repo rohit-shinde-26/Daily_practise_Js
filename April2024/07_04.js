7c7233c9acf4e465be3786a7d0f9b4025f3d0095
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-07 21:32:16 +0530

    done for today

--- April2024/07_04.js
+++ April2024/07_04.js
@@ -54,22 +54,148 @@
 // createUser.call(createUser,"Gaming","Coding")
 
 
-function info(val1,val2){
-    console.log(`my name is ${this.name} ${val1} & ${val2}`)
-}
+// function info(val1,val2){
+//     console.log(`my name is ${this.name} ${val1} & ${val2}`)
+// }
 
-const data={
-    name: "Rohit",
-    age : 22,
+// const data={
+//     name: "Rohit",
+//     age : 22,
+    
+// }
+// const data2={
+//     name: "Ram",
+//     age : 22,
+    
+// }
+// // data.about.call(data2)
+// // data.about.apply(data2,["Gaming","Coding"])
+
+// const ans = info.bind(data2,"Gaming","Coding")
+// ans()
+
+// function createUser(name,age,email){
+//     const user={}
+//     user.name=name;
+//     user.age=age;
+//     user.email=email;
+
+//     user.about=function(){
+//         return `my name is ${this.name}`
+//     };
+//     user.is18=function(){
+//         return this.age>=18
+//     }
+//     return user;
+// }
+
+// const user1 = createUser("Rohit",22,"rohit@22")
+// console.log(user1)
+
+
+// const data = {
+//     about : function(){
+//         return `my name is ${this.name}`
+//     },
+//     is18 : function(){
+//         return this.age>=18
+//     }
+// }
+
+
+// function createUser(name,age,email){
+//     const user={}
+//     user.name=name;
+//     user.age=age;
+//     user.email=email;
+//     user.about=data.about
+
+  
+//     return user;
+// }
+
+// const user1 = createUser("Rohit",22,"rohit@22")
+// console.log(user1.about())
+
+
+// __proto__
+
+// const obj = {
+//     key1 : "val1",
+//     key2 : "val2"
+// }
+// const obj2 = Object.create(obj)
+// obj2.key3 = "val3"
+// console.log(obj2.key2)
+
+
+// const data = {
+//     about : function(){
+//         return `my name is ${this.name}`
+//     },
+//     is18 : function(){
+//         return this.age>=18
+//     }
+// }
+
+
+// function createUser(name,age,email){
+//     const user=Object.create(data)
+//     user.name=name;
+//     user.age=age;
+//     user.email=email;
+    
+
+  
+//     return user;
+// }
+
+// const user1 = createUser("Rohit",22,"rohit@22")
+// console.log(user1.about())
+
+
+// prototype 
+// function info(){
+//     console.log("hello")
+// }
+// console.log(info.name)
+// info()
+
+// info.prototype.age = 22
+// console.log(info.prototype.age)
+
+
+
+// const data = {
+//     about : function(){
+//         return `my name is ${this.name}`
+//     },
+//     is18 : function(){
+//         return this.age>=18
+//     }
+// }
+
+
+function createUser(name,age,email){
+   
+    this.name=name;
+    this.age=age;
+    this.email=email;
+    
+
+  
     
 }
-const data2={
-    name: "Ram",
-    age : 22,
-    
+createUser.prototype.about=function(){    
+        return `my name is ${this.name}`
 }
-// data.about.call(data2)
-// data.about.apply(data2,["Gaming","Coding"])
 
-const ans = info.bind(data2,"Gaming","Coding")
-ans()
+const user1 = new createUser("Rohit",22,"rohit@22")
+console.log(user1.about())
+
+for(let key in user1){
+    // console.log(key)
+    if(user1.hasOwnProperty(key1)){
+        console.log(key)
+    }
+}
