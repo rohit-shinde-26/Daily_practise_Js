1d25a0e50dadfa0c5ecf5a8fa90ecc344c1093a2
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-18 17:12:33 +0530

    call apply bind

--- April2024/18_04.js
+++ April2024/18_04.js
@@ -13,4 +13,23 @@
 
 // const ans = info.about();
 
+// call
+// function myInfo(val1,val2){
+//     console.log(`my name is ${this.name} & i m ${this.age} years old with intrested in ${val1} ${val2}`)
+// }
 
+
+// const info={
+//     name : "ram",
+//     age : 22,
+   
+// }
+
+// // myInfo.call(info)
+
+// // apply
+// // myInfo.apply(info,["coding","Gaming"])
+
+// // bind 
+// const ans = myInfo.bind(info,"coding","Gaming")
+// ans()
