f1919e1eae243a41e89cde115bf7abcf0595bb4e
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-18 17:05:08 +0530

    this keyword

---
+++ April2024/18_04.js
@@ -0,0 +1,16 @@
+
+
+// function myInfo(){
+//     console.log(`my name is ${this.name} & i m ${this.age} years old`)
+// }
+
+
+// const info={
+//     name : "ram",
+//     age : 22,
+//     about : myInfo
+// }
+
+// const ans = info.about();
+
+
