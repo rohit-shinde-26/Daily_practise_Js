96c0dc39427aa6cd1660476f897768c9da64bfe2
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-25 22:37:20 +0530

    done with async

--- April2024/25_04.js
+++ April2024/25_04.js
@@ -45,21 +45,75 @@
 // })
 
 
+// const myBtn = document.querySelector("button");
+// const body = document.body;
+
+// function generateRandom(){
+
+//     const red = ~~(Math.random() * 256)
+//     const green = ~~(Math.random() * 256)
+//     const blue = ~~(Math.random() * 256)
+
+//     const rgb = `rgb(${red},${green},${blue})`
+//     return rgb
+// }
+
+// myBtn.addEventListener("click",()=>{
+//     const rgb = generateRandom();
+//     body.style.background=rgb
+    
+// })
+
+
+// console.log("script started")
+
+
+// setTimeout(()=>{
+//     console.log("one")
+//     setTimeout(()=>{
+//         console.log("two")
+//         setTimeout(()=>{
+//             console.log("three")
+//             setTimeout(()=>{
+//                 console.log("four")
+//                 setTimeout(()=>{
+//                     console.log("five")
+//                 },2000)
+//             },1000)
+//         },3000)
+//     },1000)
+// },1000)
+
+// function ex(no1,no2,add){
+//     if(typeof no1 === "number" && typeof no2 === "number"){
+//         add(no1,no2)
+//     }
+//     else{
+//         console.log("error")
+//     }
+// }
+// function addNo(no1,no2){
+//     console.log(no1+no2)
+// }
+
+// ex(2,1,addNo)
+// console.log("script end")
+
+
 const myBtn = document.querySelector("button");
 const body = document.body;
 
-function generateRandom(){
 
-    const red = ~~(Math.random() * 256)
-    const green = ~~(Math.random() * 256)
-    const blue = ~~(Math.random() * 256)
+const generate = setInterval(()=>{
+    const red = ~~(Math.random()*256);
+    const green = ~~(Math.random()*256);
+    const blue = ~~(Math.random()*256);
 
     const rgb = `rgb(${red},${green},${blue})`
-    return rgb
-}
+    console.log(rgb)
+    body.style.background = rgb;
+},1000)
 
 myBtn.addEventListener("click",()=>{
-    const rgb = generateRandom();
-    body.style.background=rgb
-    
+    clearInterval(generate)
 })
