4398bc32e1e3b994481668802cdeb85a024edc5c
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-27 19:25:08 +0530

    promise with setTimeout

--- April2024/27_04.js
+++ April2024/27_04.js
@@ -17,15 +17,38 @@
 
 // function return promise 
 
+// function myCarPromise(){
+//     const tools = ["Spoiler","Nos","Exhaust","Color"]
+//     return new Promise((resolve,reject)=>{
+//     if(tools.includes("Spoiler") && tools.includes("Exhaust")){
+//         resolve({car : "Supra"})
+//     }
+//     else{
+//         reject(error)
+//     }
+// })
+// }
+
+
+// myCarPromise().then((car)=>{
+//     console.log("This is supra",car)
+// }).catch((error)=>{
+//     console.log("This is not supra")
+// })
+
+// setTimeout in promise 
+
 function myCarPromise(){
     const tools = ["Spoiler","Nos","Exhaust","Color"]
     return new Promise((resolve,reject)=>{
-    if(tools.includes("Spoiler") && tools.includes("Exhaust")){
-        resolve({car : "Supra"})
-    }
-    else{
-        reject(error)
-    }
+    setTimeout(()=>{
+        if(tools.includes("Spoiler") && tools.includes("Exhaust")){
+            resolve({car : "Supra"})
+        }
+        else{
+            reject(error)
+        }
+    },2000)
 })
 }
 
