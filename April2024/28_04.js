bcb5312ab667e5a75d786ee975d359f99af23e6a
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-28 20:22:13 +0530

    done for the day

---
+++ April2024/28_04.js
@@ -0,0 +1,26 @@
+// console.log("Hello world")
+
+// var a = "Ram"
+// var a = "Sham"
+// a = "hanuman"
+// console.log(a)
+
+// let myName = "Rohit"
+// myName ="Abhi"
+// console.log(myName)
+
+// console.log(myName[2])
+
+// console.log(myName.toUpperCase())
+
+// let num = false;
+// if(num){
+//     console.log("Truthy value")
+// }else{
+//     console.log("Falsy value")
+// }
+
+
+// let age = 20
+// const ans = age<18 ? "You are teenager" : "You are young";
+// console.log(ans)
