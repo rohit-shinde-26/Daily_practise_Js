14fcc5c169692f6643765697378ca327af6cae8b
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-29 23:33:22 +0530

    array done for the day

---
+++ April2024/29_04.js
@@ -0,0 +1,31 @@
+// Arrays
+const myArr = ["Ram","Sham","Hari"]
+
+// myArr.push("Om")
+// myArr.unshift("Abhi")
+// console.log(myArr)
+
+// const [...newArr]=myArr;
+
+// console.log(newArr)
+// console.log(newArr === myArr)
+
+
+// let i = 0;
+
+// while(i<myArr.length){
+//     console.log(myArr[i])
+//     i++
+// }
+
+// for (let ele of myArr){
+//     console.log(ele)
+// }
+
+
+// for (let ele in myArr){
+//     console.log(myArr[ele])
+// }
+
+// const [name1,name2]=myArr;
+// console.log(name1)
