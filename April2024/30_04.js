a98f6767d776f8bafa77937d4716423b4893c378
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-30 21:25:17 +0530

    done for the day

---
+++ April2024/30_04.js
@@ -0,0 +1,22 @@
+// const myBtn = document.querySelector("button")
+// const body = document.body
+
+
+// const data = setInterval(()=>{
+
+//     const red = ~~(Math.random()*256);
+//     const green = ~~(Math.random()*256);
+//     const blue = ~~(Math.random()*256);
+//     const rgb = `rgb(${red},${green},${blue})`
+//     body.style.background = rgb
+// },1000)
+
+// myBtn.addEventListener("click",()=>{
+//     clearInterval(data)
+// })
+
+
+
+function createUser(name,age,is18,){
+    
+}
