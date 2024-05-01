055aab5d822b94d5e460dffab433525f8686bb1a
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-05-01 10:25:49 +0530

    callback hell

--- May2024/01_05.js
+++ May2024/01_05.js
@@ -35,21 +35,45 @@
 // },100)
 
 
-const btn = document.querySelector("button");
-const body = document.body;
+// const btn = document.querySelector("button");
+// const body = document.body;
 
-const generator = setInterval(()=>{
+// const generator = setInterval(()=>{
 
-    const red = ~~(Math.random()*256);
-    const green = ~~(Math.random()*256);
-    const blue = ~~(Math.random()*256);
+//     const red = ~~(Math.random()*256);
+//     const green = ~~(Math.random()*256);
+//     const blue = ~~(Math.random()*256);
 
-    const rgb = `rgb(${red},${green},${blue})`
-    body.style.backgroundColor=rgb;
+//     const rgb = `rgb(${red},${green},${blue})`
+//     body.style.backgroundColor=rgb;
+// },1000)
+
+// btn.addEventListener("click",()=>{
+//     clearInterval(generator)
+// })
+
+// console.log(generator)
+
+
+
+const heading1 = document.querySelector(".heading1")
+const heading2 = document.querySelector(".heading2")
+const heading3 = document.querySelector(".heading3")
+const heading4 = document.querySelector(".heading4")
+const heading5 = document.querySelector(".heading5")
+const heading6 = document.querySelector(".heading6")
+const heading7 = document.querySelector(".heading7")
+const heading8 = document.querySelector(".heading8")
+const heading9 = document.querySelector(".heading9")
+const heading10 = document.querySelector(".heading10")
+
+
+
+setTimeout(()=>{
+    heading1.textContent="One"
+    heading1.style.color="red"
+    setTimeout(()=>{
+        heading2.textContent="two"
+        heading2.style.color="yellow"
+    },1000)
 },1000)
-
-btn.addEventListener("click",()=>{
-    clearInterval(generator)
-})
-
-console.log(generator)
