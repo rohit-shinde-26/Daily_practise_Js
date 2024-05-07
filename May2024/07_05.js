97a79bd0997c68d53d1eb9bf81b1d85fa7537fa1
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-05-07 20:59:18 +0530

    async starter

---
+++ May2024/07_05.js
@@ -0,0 +1,23 @@
+// const data = setTimeout(()=>{
+//     console.log("Hello")
+// },1000)
+
+
+// const getNumAdd = function(num1,num2,onSuccess,onFailure){
+//     if(typeof num1 === "number" && typeof num2 === "number"){
+//         onSuccess(num1,num2);
+//     }else{
+//         onFailure()
+//     }
+
+// }
+
+// function adds(num1,num2){
+//     console.log(num1+num2)
+
+// }
+// function err(){
+//     console.log(error)
+// }
+
+// const ans = getNumAdd(5,6,adds,err)
