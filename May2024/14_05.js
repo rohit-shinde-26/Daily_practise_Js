8327fd8c6b88d44605616ad0a42c83c5ec3afb09
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-05-14 20:18:18 +0530

    done today with the useful things

---
+++ May2024/14_05.js
@@ -0,0 +1,47 @@
+// Iterables
+// the data who are takes the length property also for of loop
+
+// let str = "Ram";
+// console.log(str.length);
+// for (let len of str) {
+//   console.log(len);
+// }
+
+// let arr = ["Ram", "Sham", "Abhi"];
+// console.log(arr.length);
+// for (let ele of arr) {
+//   console.log(ele);
+// }
+
+// Sets
+// it is iterables
+
+// let myArr = new Set();
+// myArr.add("Abhi");
+// myArr.add("Akshay");
+
+// console.log(myArr);
+
+// Maps
+// let myApp = new Map([
+//   ["Abhi", 22],
+//   ["Ram", 18],
+// ]);
+// // myApp.set("Akshay", 22);
+
+// console.log(myApp);
+
+// Object assign
+// it is the technique of cloning
+
+let myObj = {
+  name: "ram",
+  age: 22,
+  //   address: { house: "sangamnerkh22" },
+};
+
+console.log(myObj.name);
+console.log(myObj?.address?.house);
+
+// const newObj = Object.assign(myObj);
+// console.log(newObj);
